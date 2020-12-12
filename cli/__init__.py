918f818aba895927b399b43b705eb83574f0cbaa
godmind <aristide.bamazi@imsp-uac.org>
2020-12-12 20:45:48 +0100

    + add of Site Model to project

---
+++ cli/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URI = os.path.dirname(os.path.abspath(__file__)) + r"/src/database/sqlite.db"
---
+++ sitemgr.py
@@ -0,0 +1,7 @@
+from cli.src.database.model import Site, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    pass
